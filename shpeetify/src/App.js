bfd68689f337f794d862dfc4eb2edfbd7b3b63ca
Shako Kushashvili <shako.kushashvili@gmail.com>
2022-08-28 00:08:45 -0400

    Added DataLayerContext & project outline

--- shpeetify/src/App.js
+++ shpeetify/src/App.js
@@ -1,11 +1,16 @@
 import React, {useEffect, useState} from 'react'
 import './App.css';
 import Login from './Login'
+import Player from './Player';
 import { getTokenFromUrl} from './spotify'
+import SpotifyWebApi from "spotify-web-api-js"
+import { useDataLayerValue } from './DataLayer';
+
+const spotify = new SpotifyWebApi //connecting our access token so spotify can communicate to our react project
 
 function App() {
 
-  const [token,setToken] = useState(null);
+  const [{user, token}, dispatch] = useDataLayerValue(); //special tool to pass to the data layer to update values
 
   //runs the code on a specific condition
   useEffect(() => {
@@ -14,16 +19,33 @@ function App() {
     const _token = hash.access_token;
 
     if(_token){
-      setToken(_token)
+
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token
+      })
+
+      
+      spotify.setAccessToken(_token)
+
+      spotify.getMe().then(user => {
+        console.log(user)
+
+        dispatch({ //pops user right into the data layer
+          type: "SET_USER",
+          user: user
+        })
+      }) //returns user account info
     }
-    console.log(token)
+
+    // console.log(token)
   }, []);
 
   return (
     <div className="App">
       {
         token ? (
-          <h1>Im IN :D</h1>
+          <Player spotify={spotify} />
         ) : (
           <Login />
         )
